8635d6191b55cccc22f659fed5bc210ba2b26108
Emiljano <Emiljanokurbiba@gmail.com>
2020-06-10 11:56:36 +0100

    Templates are definatly required
    So figured out templates, seems simple enough, added about, home, layout, login & register still running on localhost.

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
-from flask import Flask
+from application import app
 
-app = Flask(__name__)
-
-@app.route('/')
-@app.route('/home')
-def home():
-    return "Flask Stage 1: Testing simple output & Making sure flask runs and is operative, currently running on localhost"
+if __name__ == '__main__':
+    app.run(debug=True)
