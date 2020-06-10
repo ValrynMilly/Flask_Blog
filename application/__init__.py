0d8b459fa4c9a34a62af05e4c840d6a2c393080c
Emiljano <Emiljanokurbiba@gmail.com>
2020-06-10 15:21:18 +0100

    Revert "sum 3"
    This reverts commit 1278eadf5c353ae9f18a2aed6410dd77b606c7ac.

--- application/__init__.py
+++ application/__init__.py
@@ -1,5 +1,4 @@
 from flask import Flask
-<<<<<<< HEAD
 from flask_sqlalchemy import SQLAlchemy
 import os
 
@@ -9,9 +8,3 @@ app.config['SQLALCHEMY_DATABASE_URI'] = str(os.getenv('DATABASE_URI'))
 db = SQLAlchemy(app)
 
 from application import routes
-=======
-
-app = Flask(__name__)
-
-from application import routes
->>>>>>> parent of f4e70fa... stage 3
