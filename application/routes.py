cf38b2804faf0d286a22e6de48b65d3209aa1f9b
Emiljano <Emiljanokurbiba@gmail.com>
2020-06-10 15:24:09 +0100

    Reverted some changes back to stage 2
    Back2Stage2

--- application/routes.py
+++ application/routes.py
@@ -2,10 +2,30 @@ from application import app
 from flask import render_template
 
 
+dummyData = [
+{
+"name": {"first":"Smash", "last":"Mouth"},
+"title":"All Star",
+"content":"Somebody once told me the world is gonna roll me"
+"\n I ain't the sharpest tool in the shed"
+"\n She was looking kind of dumb with her finger and her thumb"
+"\n In the shape of an ""L"" on her forehead"
+},
+{
+"name": {"first":"Monty", "last":"Python"},
+"title":"Herring",
+"content":"when you have found the shrubbery, you must cut down the mightiest tree in the forest... with... a herring!"
+},
+{
+"name": {"first":"Jules", "last":"Winnfield"},
+"title":"Big Kahuna Burger",
+"content":"Do you know why they call it a royale with cheese? Because of the Metric System? CHECK OUT THE BIG BRAIN ON BRETT"
+}
+]
+
 @app.route('/')
 @app.route('/home')
 def home():
-    postData = Posts.query.all()
     return render_template('home.html', title='Home', posts=dummyData)
 
 @app.route('/about')
