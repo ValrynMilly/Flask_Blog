ab309e0759eedd20e903fde85c48d6521f7b8489
Emiljano <Emiljanokurbiba@gmail.com>
2020-06-10 13:57:09 +0100

    Stage 2 Routing
    Ive added some dummy date into the home page, its a meme-fest but working nonetheless

--- application/routes.py
+++ application/routes.py
@@ -1,13 +1,27 @@
-
-
-
 from application import app
 from flask import render_template
+
+
+dummyData = [
+{
+"name": {"first":"Smash", "last":"Mouth"},
+"title":"All Star",
+"content":"Somebody once told me the world is gonna roll me"
+"\n I ain't the sharpest tool in the shed"
+"\n She was looking kind of dumb with her finger and her thumb"
+"\n In the shape of an ""L"" on her forehead"
+},
+{
+"name": {"first":"Monty", "last":"Python"},
+"title":"Herring",
+"content":"when you have found the shrubbery, you must cut down the mightiest tree in the forest... with... a herring!"
+}
+]
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html', title='Home')
+    return render_template('home.html', title='Home', posts=dummyData)
 
 @app.route('/about')
 def about():
