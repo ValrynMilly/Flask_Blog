cae837649282f826ac4d6ba38fdeeda4c9df863d
Emiljano <Emiljanokurbiba@gmail.com>
2020-06-11 22:11:52 +0100

    Update __init__.py

--- application/__init__.py
+++ application/__init__.py
@@ -1,17 +1,17 @@
-import os
-from flask_bcrypt import Bcrypt
 from flask import Flask
-from os import getenv
 from flask_sqlalchemy import SQLAlchemy
+from os import getenv
+from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
 app = Flask(__name__)
-login_manager.login_view = 'login'
 app.config['SECRET_KEY']='Thisissecret'
 app.config['SQLALCHEMY_DATABASE_URI'] = str(os.getenv('DATABASE_URI'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
+login_manager =LoginManager(app)
+login_manager.login_view = 'login'
 
 from application import routes
