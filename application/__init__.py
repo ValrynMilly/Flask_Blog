3498cab25d415938810e3830b5e06babad00d376
ValrynMilly <emiljanokurbiba@gmail.com>
2020-06-11 13:25:20 +0000

    Front & backend done finally

--- application/__init__.py
+++ application/__init__.py
@@ -1,9 +1,10 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 import os
+from flask import Flask
+from os import getenv
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY']='Thisissecret'
 app.config['SQLALCHEMY_DATABASE_URI'] = str(os.getenv('DATABASE_URI'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
--- application/routes.py
+++ application/routes.py
@@ -1,6 +1,31 @@
+from flask import render_template, redirect, url_for
+from application import app, db
+from application.forms import PostForm
 from application.models import Posts
 from application import app
 from flask import render_template
+
+
+
+@app.route('/post', methods=['GET', 'POST'])
+def post():
+    form = PostForm()
+    if form.validate_on_submit():
+        postData = Posts(
+                first_name = form.first_name.data,
+                last_name = form.last_name.data,
+                title = form.title.data,
+                content = form.content.data
+        )
+
+        db.session.add(postData)
+        db.session.commit()
+
+        return redirect(url_for('home'))
+    else:
+        print(form.errors)
+
+    return render_template('post.html', title='Post', form=form)
 
 @app.route('/')
 @app.route('/home')
