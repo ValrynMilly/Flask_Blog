03fdd9226341de365266dd037ca691094edc7709
ValrynMilly <emiljanokurbiba@gmail.com>
2020-06-11 21:03:54 +0000

    merge

--- application/__init__.py
+++ application/__init__.py
@@ -7,11 +7,11 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
 app.config['SECRET_KEY']='Thisissecret'
 app.config['SQLALCHEMY_DATABASE_URI'] = str(os.getenv('DATABASE_URI'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
 from application import routes
