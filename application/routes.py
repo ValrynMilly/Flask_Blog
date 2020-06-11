692552733e5908df79d62017521adf788774a49b
Emiljano <Emiljanokurbiba@gmail.com>
2020-06-11 21:51:51 +0100

    updated routes.py

--- application/routes.py
+++ application/routes.py
@@ -1,9 +1,10 @@
 from flask import render_template, redirect, url_for
+from flask_login import login_user, current_user, logout_user, login_required
 from application import app, db
-from application.forms import PostForm, RegistrationForm
+from application.forms import PostForm, RegistrationForm, LoginForm
 from application.models import Posts
 from application import app
-from flask import render_template
+from flask import render_template, redirect, url_for, request
 
 
 
@@ -51,7 +52,19 @@ def register():
         return redirect(url_for('post'))
     return render_template('register.html', title='Register', form=form)
     
-@app.route('/login')
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    return render_template('login.html', title='Login')
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user=Users.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            if next_page:
+                return redirect(next_page)
+            else:
+                return redirect(url_for('home'))
+    return render_template('login.html', title='Login', form=form)
 
